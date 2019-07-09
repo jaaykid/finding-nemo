aa57b4e0213a9e47d4eb5788c8778cfb5e162e79
jaay_kid <justincurrydev@gmail.com>
2019-07-08 18:18:51 -0700

    implemented clarifai api to create face recognition box, added sign in and register forms

--- src/components/ImageForm/ImageForm.js
+++ src/components/ImageForm/ImageForm.js
@@ -2,16 +2,16 @@ import React from 'react'
 import './ImageForm.css'
 
 
-const ImageForm = () => {
+const ImageForm = ({ onInputChange, onSubmit }) => {
   return (
     <div>
       <p className="f3">
-        {'This Magic Eye will detect faces in your pictures. Give it a try'}
+        {'This Magic Eye will detect faces in your pictures. Give it a try.'}
       </p>
       <div className='center'>
       <div className='form center pa4 br3 shadow-5'>
-        <input className='f4 pa2 w-70 center' type="text" name="" id=""/>
-        <button className='w-30 grow f4 link ph3 pv2 dib white bg-yellow'>Detect</button>
+        <input className='f4 pa2 w-70 center' type="text" onChange={onInputChange}/>
+        <button className='w-30 grow f4 link ph3 pv2 dib white bg-yellow' onClick={onSubmit} >Detect</button>
       </div>
       </div>
     </div>
